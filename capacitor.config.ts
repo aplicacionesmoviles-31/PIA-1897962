7916ad0dc5f42bab7f36b1e65dfd7ad7fe082d7c
JM1897962 <1897962.jmmr@gmail.com>
2022-05-17 19:39:04 -0500

    PIA-1897962-JMMR

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.democapacitor.fcfm',
-  appName: 'ionic-basic',
+  appName: 'PIA-1897962',
   webDir: 'www',
   bundledWebRuntime: false
 };
